4faeed77ae18b01aacd39afbd463844a82fe8a1e
Paulo Victor Vasconcelos de Lacerda <fpl.lacerda99@gmail.com>
2019-12-01 17:50:04 -0300

    Componente de cabeçalho.

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,17 @@
+/**
+ * Componente Header - Cabeçalho da aplicação e responsável pelos botões de controle do tamanho da fonte.
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mf-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
