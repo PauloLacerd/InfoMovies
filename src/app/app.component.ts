9418afc7b431eb4cc050e7ca6aaef90683578d0f
Paulo Victor Vasconcelos de Lacerda <fpl.lacerda99@gmail.com>
2019-12-05 20:08:23 -0300

    Componente principal da aplicação.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NgxSpinnerService } from 'ngx-spinner'
 
 @Component({
   selector: 'mf-root',
@@ -7,7 +6,7 @@ import { NgxSpinnerService } from 'ngx-spinner'
 })
 export class AppComponent implements OnInit {
 
-  constructor(private spinner: NgxSpinnerService) {
+  constructor() {
   }
 
   ngOnInit() {
